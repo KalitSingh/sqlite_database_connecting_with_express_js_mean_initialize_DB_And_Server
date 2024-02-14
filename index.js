25aca02cd4816bb646ae73e0a0c081c51efc7b5b
KalitSingh <kalitsingh9935@gmail.com>
2024-02-14 10:49:23 +0000

    Adding one more API to get details of a single bookd using sqlite method 'get' and path parameter by aaccessing it from request using 'request.params'

--- index.js
+++ index.js
@@ -7,27 +7,38 @@ const {open} = require('sqlite')
 const path = require('path')
 const dbPath = path.join(__dirname, './goodreads.db')
 let db = null
-const initilizeDBAndServer = async () => {
+const initializeDBAndServer = async () => {
   try {
     db = await open({
       filename: dbPath,
       driver: sqlite3.Database,
     })
     app.listen(3000, () =>
-      console.log('Server is Running at http:localhost:3000/'),
+      console.log('Server is Running at http://localhost:3000/'),
     )
   } catch (error) {
-    console.log(`DB Error ${error.message}`)
-    process.exit(1)
+    console.log(`Db Error ${error.message}`)
   }
 }
 
-initilizeDBAndServer()
+initializeDBAndServer()
 
+// Writting an API to get details of all rows of the books
 app.get('/books/', async (request, response) => {
-  const bookQuery = `
+  const getBookQuery = `
     SELECT * FROM book;
   `
-  const bookArray = await db.all(bookQuery)
+  const bookArray = await db.all(getBookQuery)
   response.send(bookArray)
+})
+
+// Writting API for a single book using get() instead of all()
+app.get('/books/:bookId/', async (request, response) => {
+  const {bookId} = request.params
+  const bookQueryById = ` 
+  SELECT * FROM book 
+  WHERE book_id = ${bookId};
+  `
+  const singleBookDetail = await db.get(bookQueryById)
+  response.send(singleBookDetail)
 })
